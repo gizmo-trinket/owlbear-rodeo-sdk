54543240b2a352a7efe8e7c29e4f5d38c3de08c4
Jeffrey Cochran <jeffrey-cochran@users.noreply.github.com>
2025-08-08 12:45:31 -0400

    modify messageBus to listen to OBR_READY from iframe

--- src/messages/MessageBus.ts
+++ src/messages/MessageBus.ts
@@ -29,17 +29,18 @@ class MessageBus extends EventEmitter {
     const message = event.data;
     // Ensure the message is meant for us and check that it is formatted correctly
     if (event.origin === this.targetOrigin && isMessage(message)) {
-      // Handle the ready event
-      if (message.id === "OBR_READY") {
-        this.ready = true;
-        console.log("READY!");
-        const data = message.data as { userId: string; ref: string };
-        this.ref = data.ref;
-        this.userId = data.userId;
-      }
       this.emit(message.id, message.data);
     }
-  };
+    else if (!this.ready && isMessage(message) && message.id === "OBR_READY") {
+      // Handle the ready event
+      this.ready = true;
+      const data = message.data as { userId: string; ref: string };
+      this.ref = data.ref;
+      this.userId = data.userId;
+      this.emit(message.id, message.data);
+    }
+  }
+};
 
   /**
    * @param nonce
